3e160c622c69d88f1d23257fd25d3a755f5f9c05
unknown <danny.chen@willtechmacau.com>
2021-12-28 19:33:43 +0800

    20211228

--- ocr.py
+++ ocr.py
@@ -8,16 +8,16 @@ import time
 # 1.图片高斯模糊去噪点
 # 2.转换成灰度图，增强文字轮廓
 # 3.切割关键部分，提高识别效率，和识别率
-def chuli(image):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # 大津法二值化
-    # retval, dst = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-    # # 腐蚀和膨胀是对白色部分而言的，膨胀，白区域变大，最后的参数为迭代次数
-    # dst = cv2.dilate(dst, None, iterations=1)
-    # # 腐蚀，白区域变小
-    # dst = cv2.erode(dst, None, iterations=2)
-    # return dst
-    return gray
+# def chuli(image):
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     # 大津法二值化
+#     # retval, dst = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+#     # # 腐蚀和膨胀是对白色部分而言的，膨胀，白区域变大，最后的参数为迭代次数
+#     # dst = cv2.dilate(dst, None, iterations=1)
+#     # # 腐蚀，白区域变小
+#     # dst = cv2.erode(dst, None, iterations=2)
+#     # return dst
+#     return gray
 
 def boxDraw(img,boxes):
     temp = boxes.split('\n')
@@ -30,6 +30,10 @@ def boxDraw(img,boxes):
 
 def dataDraw(img,data):
     temp = data.split('\n')
+    print(type(img))
+    t = []
+    max_width = 0
+    max_height = 0
     for index,temp1 in enumerate(temp):
         temp2 = temp1.split()
         if index==0:
@@ -44,30 +48,34 @@ def dataDraw(img,data):
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255))
             #把对应的图片都给扣下来
             imgcrop = img[y1:y2,x1:x2]
+            print(imgcrop.shape)
+            t.append(imgcrop)
             filename = "result_"+str(index)+".png"
-            cv2.imwrite(filename,imgcrop)
+    print(len(t))
+    result = cv2.vconcat(t)
+    cv2.imshow("result",result)
 
+class OcrSdk:
+    def start(self,img):
+      
+        custom_config = r'-l chi_sim eng equ --oem 3 --psm 7' 
+        result = pytesseract.image_to_string(img, config=custom_config)
+        boxes  = pytesseract.image_to_boxes(img)
+        data = pytesseract.image_to_data(img)
+        print(result)
+        dataDraw(img,data)
+        cv2.imshow('ocr_result',img)
+        cv2.waitKey()
 
-img = cv2.imread('c2.png')
+obj = OcrSdk()
+filename = '1111.png'
+img = cv2.imread(filename)
 img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
-img = chuli(img)
+obj.start(img)
 
-custom_config = r'-l chi_sim eng --oem 3 --psm 7' 
-result = pytesseract.image_to_string(img, config=custom_config)
-boxes  = pytesseract.image_to_boxes(img)
-data = pytesseract.image_to_data(img)
-print(result)
-# print(result)
-dataDraw(img,data)
-# boxDraw(img,boxes)
 
-# print(type(data))
-# for box in boxes:
-#     print(box)
-#     cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(255,0,0))
 
-cv2.imshow('temp',img)
-cv2.waitKey()
+
 
 
 
--- side_increase.py
+++ side_increase.py
@@ -2,18 +2,23 @@ import cv2
 import os
 import numpy as np
 
+from ocr import OcrSdk
+
 MODE = 1 #0表示普通识别，1表示算法优化过的
 #使用canny对边缘进行加强
 
 
 def order_points(pts):
-    # 初始化坐标点
+   # 一共4个坐标点
     rect = np.zeros((4, 2), dtype = "float32")
-    # 获取左上角和右下角坐标点
+ 
+    # 按顺序找到对应坐标0123分别是 左上，右上，右下，左下
+    # 计算左上，右下
     s = pts.sum(axis = 1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
-    # 分别计算左上角和右下角的离散差值
+ 
+    # 计算右上和左下
     diff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
@@ -22,83 +27,94 @@ def order_points(pts):
 
 #仿射变换，把倾斜的图片转换成水平
 def four_point_transform(image, pts):
-
-    # 获取坐标点，并将它们分离开来
+    # 获取输入坐标点
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
-    # 计算新图片的宽度值，选取水平差值的最大值
+    print(rect)
+    # 计算输入的w和h值
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-
+    print("------------")
+    print(widthA)
+    print(widthB)
     maxWidth = max(int(widthA), int(widthB))
-
-    # 计算新图片的高度值，选取垂直差值的最大值
-
+    print(maxWidth)
+    
+    print("------------")
+ 
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 
+    print("------------")
+    print(heightA)
+    print(heightB)
     maxHeight = max(int(heightA), int(heightB))
-
-    # 构建新图片的4个坐标点
-
-    dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
-
-    # 获取仿射变换矩阵并应用它
-
+    print(maxHeight)
+    print("------------")
+ 
+    # 变换后对应坐标位置
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    print("矩阵dst：")
+    print(dst)
+    # 计算变换矩阵
     M = cv2.getPerspectiveTransform(rect, dst)
-
-    # 进行仿射变换
-
+    print(M)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # 返回变换后的结果
-
+    # cv2.imshow('wrapper',warped)
+    # 返回变换后结果
     return warped
 
 
+
 def start(mode):
-    img = cv2.imread('shiji.png')
-    img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
+    img = cv2.imread('crop_1.png')
+    img = cv2.resize(img,(0,0),fx=0.6,fy=0.6)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if mode==1 :
-        canny=cv2.Canny(gray,30,140,3)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        dilate = cv2.dilate(blurred, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+        canny=cv2.Canny(dilate,30,120,3)
         #将边缘增强过的图片的外部轮廓提取出来
-        contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        maxarea=0
-        maxint=0
-        i = 0
-        for c in contours:
-            if cv2.contourArea(c) > maxarea:
-                maxarea=cv2.contourArea(c)
-                maxint = i
-                i +=1
-        box = cv2.approxPolyDP(contours[maxint], 15, True) #多边形拟合 True 代表封闭
-        print(box.shape)
-        poly = np.zeros(canny.shape)
-        cv2.polylines(poly, [box],True, (255, 0, 0)) #连线
-        cv2.imshow("2",poly)
-        warped = four_point_transform(img, box)
-        cv2.imshow("Warped", warped)
-
+        contours, hierarchy = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        docCnt = None
+        if len(contours) > 0:
+        # 按轮廓大小降序排列
+            cnts = sorted(contours, key=cv2.contourArea, reverse=True)
+            print("c len:"+str(len(cnts)))
+            for c in cnts:
+                # 近似轮廓
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+                # 如果我们的近似轮廓有四个点，则确定找到了纸
+                if len(approx) == 4:
+                    docCnt = approx
+                    break
+        # 对原始图像应用四点透视变换，以获得纸张的俯视图
+        paper = four_point_transform(img, docCnt.reshape(4, 2))
+        #调用tessract进行文字识别
+        OcrSdk().start(paper)
     else:
         ret,result = cv2.threshold(gray,127,255,cv2.THRESH_OTSU)
         contours, hierarchy = cv2.findContours(result, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
 
     # 在原图上面或轮廓
-    draw_img0 = cv2.drawContours(img,contours,-1,(0,255,255),3)
-    if mode==1:
-        cv2.imshow("canny",img)
-    else:
-        cv2.imshow("normal",draw_img0) 
-        # 找出矩形对应的框
-        bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
-        # #在图片上画框
-        for bbox in bounding_boxes:
-            [x , y, w, h] = bbox
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # draw_img0 = cv2.drawContours(img,contours,-1,(0,255,255),3)
+    # if mode==1:
+    #     cv2.imshow("canny",img)
+    # else:
+    #     cv2.imshow("normal",draw_img0) 
+    #     # 找出矩形对应的框
+    #     bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+        
+    #     # #在图片上画框
+    #     for bbox in bounding_boxes:
+    #         [x , y, w, h] = bbox
+    #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.waitKey()
 
 start(MODE)
